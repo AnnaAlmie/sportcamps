511ef1d6289c6aa0a7c13eb84c3366f1912038c4
AnnaAlmie <annalitvinenko9@gmail.com>
2020-09-16 23:16:20 +0300

    перед правки

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ function fullPage() {
     $('#fullpage').fullpage({
         sectionSelector: '.vertical-scrolling',
         slidesNavigation: false,
-        anchors: ['main', 'advantages', 'advantages-slider', 'сases', 'сamps', 'how', 'faq'],
+        anchors: ['main', 'advantages', 'advantages-slider', 'сamps', 'сases', 'models', 'models-slider', 'how', 'how-slider', 'faq'],
         menu: '#menu',
         keyboardScrolling: true,
         controlArrows: true,
@@ -46,17 +46,19 @@ function wrapCases() {
 /*-------------------Sliders----------------------*/
 
 function sliderVerticle() { //full page 
-    $(".slider__verticle").slick({
-        lazyLoad: 'ondemand',
-        infinite: true,
-        // fade: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        prevArrow: '<div class="slider__arrow slider__arrow__prev"></div>',
-        nextArrow: '<div class="slider__arrow slider__arrow__next"></div>',
-        vertical: true
+    $(".slider__verticle").each(function(index) {
+        $(this).slick({
+            lazyLoad: 'ondemand',
+            infinite: true,
+            // fade: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            prevArrow: '<div class="slider__arrow slider__arrow__prev"></div>',
+            nextArrow: '<div class="slider__arrow slider__arrow__next"></div>',
+            vertical: true
+        })
     })
 }
 
@@ -79,3 +81,9 @@ function sliderCases() { //962 height
 }
 
 /*-----------------------------------------*/
+
+
+// faq 
+$('.slide__block').click(function() {
+    $(this).toggleClass('slide__block__open');
+})
